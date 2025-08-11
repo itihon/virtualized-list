688b6608982bf99b9fd2e46df936cba3c3fb57f3
itihon <kalabin.alexandr@gmail.com>
2025-08-11 15:12:48 +0500

    test: add a draft for RangeTree unit test

---
+++ cypress/unit/RangeTree.cy.ts
@@ -0,0 +1,70 @@
+// --------- range tree test
+
+
+// const tree = new RangeTree();
+// 
+// tree.insert(0, { size: 2, item: new VirtualizedList() });
+// tree.insert(1, { size: 3, item: new VirtualizedList() });
+// tree.insert(2, { size: 8, item: new VirtualizedList() });
+// tree.insert(3, { size: 12, item: new VirtualizedList() });
+// tree.insert(4, { size: 5, item: new VirtualizedList() });
+// tree.insert(5, { size: 14, item: new VirtualizedList() });
+// tree.insert(6, { size: 6, item: new VirtualizedList() });
+// tree.insert(7, { size: 4, item: new VirtualizedList() });
+// tree.insert(8, { size: 9, item: new VirtualizedList() });
+// tree.insert(9, { size: 7, item: new VirtualizedList() });
+// tree.insert(10, { size: 16, item: new VirtualizedList() });
+// tree.insert(11, { size: 11, item: new VirtualizedList() });
+// tree.insert(12, { size: 13, item: new VirtualizedList() });
+// tree.insert(13, { size: 15, item: new VirtualizedList() });
+// tree.insert(14, { size: 17, item: new VirtualizedList() });
+// 
+// 
+// 
+// console.log(tree.toString());
+// 
+// tree.insert(15, { size: 17, item: new VirtualizedList() });
+// 
+// console.log(tree.toString());
+// 
+// tree.insert(16, { size: 17, item: new VirtualizedList() });
+// 
+// console.log(tree.toString());
+// 
+// tree.insert(17, { size: 17, item: new VirtualizedList() });
+// // tree.insert(2, { size: 18, item: new VirtualizedList() });
+// 
+// console.log(tree.toString());
+// 
+// 
+// console.log(1, tree.findByOffset(1));
+// console.log(2, tree.findByOffset(2));
+// console.log(3, tree.findByOffset(3));
+// console.log(5, tree.findByOffset(5));
+// console.log(7, tree.findByOffset(7));
+// console.log(13, tree.findByOffset(13));
+// console.log(15, tree.findByOffset(15));
+// console.log(25, tree.findByOffset(25));
+// console.log(28, tree.findByOffset(28));
+// console.log(30, tree.findByOffset(30));
+// console.log(32, tree.findByOffset(32));
+// console.log(44, tree.findByOffset(44));
+// console.log(48, tree.findByOffset(48));
+// console.log(50, tree.findByOffset(50));
+// console.log(52, tree.findByOffset(52));
+// console.log(54, tree.findByOffset(54));
+// console.log(58, tree.findByOffset(58));
+// console.log(63, tree.findByOffset(63));
+// console.log(65, tree.findByOffset(65));
+// console.log(70, tree.findByOffset(70));
+// console.log(85, tree.findByOffset(85));
+// console.log(86, tree.findByOffset(86));
+// console.log(96, tree.findByOffset(96));
+// console.log(97, tree.findByOffset(97));
+// console.log(98, tree.findByOffset(98));
+// console.log(110, tree.findByOffset(110));
+// console.log(124, tree.findByOffset(124));
+// console.log(125, tree.findByOffset(125));
+// console.log(130, tree.findByOffset(130));
+// console.log(142, tree.findByOffset(142));
+// console.log(200, tree.findByOffset(200));
