ba031bc59267f984cafe811d82f70cc01c8ec6a9
itihon <kalabin.alexandr@gmail.com>
2025-08-16 22:19:06 +0500

    chore(vite.manual.config.js): make vite listen all available network interfaces

--- vite.manual.config.js
+++ vite.manual.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   return {
     root: './cypress/manual/',
+    server: {
+      host: '0.0.0.0',
+    },
   };
 });
