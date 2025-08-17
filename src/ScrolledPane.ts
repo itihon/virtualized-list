8a8edfbbe83e4b89006ec243469d0bf64f168855
itihon <kalabin.alexandr@gmail.com>
2025-08-17 19:07:11 +0500

    feat(scrolledpane.ts): add append(), prepend(), removeItem(), length, DOMRoot

--- src/ScrolledPane.ts
+++ src/ScrolledPane.ts
@@ -38,4 +38,35 @@ export default class ScrolledPane {
   get scrollUpLimit(): number {
     return this._scrollUpLimit;
   }
+
+  append(...items: HTMLElement[]) {
+    for (const item of items) {
+      this._paneElement.append(item);
+    }
+  }
+  
+  prepend(...items: HTMLElement[]) {
+    for (const item of items) {
+      this._paneElement.prepend(item);
+    }
+  }
+
+  removeItem(itemIndex: number): boolean {
+    const item = this._paneElement.children.item(itemIndex);
+
+    if (item) {
+      item.remove();
+      return true;
+    }
+
+    return false;
+  }
+
+  get length(): number {
+    return this._paneElement.children.length;
+  }
+
+  get DOMRoot(): HTMLElement {
+    return this._paneElement;
+  }
 }
