279a9569b91883ee14ac69122bc85d28b9086efd
itihon <kalabin.alexandr@gmail.com>
2025-08-17 12:29:22 +0500

    refactor(ScrollableContainer.ts): move class Filler in a separate module

--- src/ScrollableContainer.ts
+++ src/ScrollableContainer.ts
@@ -1,4 +1,5 @@
 import ScrolledPane from './ScrolledPane';
+import Filler from './Filler';
 import './ScrollableContainer.css';
 
 export type OnScrollLimitCallback = (scrolledPane: ScrolledPane, scrollTop: number) => void;
@@ -38,33 +39,6 @@ class BooleanToggle {
     this._value = value;
 
     return result;
-  }
-}
-
-class Filler {
-  private _scrollableParent: HTMLElement;
-  private _fillerElement: HTMLElement;
-  private _offsetHeight: number = 0;
-
-  constructor(scrollableParent: HTMLElement) {
-    this._scrollableParent = scrollableParent;    
-    this._fillerElement = document.createElement('div');
-    this._fillerElement.classList.add('class__Filler');
-    this._scrollableParent.appendChild(this._fillerElement);
-  }
-
-  set offsetHeight(height: number) {
-    const roundHeight = Math.round(height)
-    this._fillerElement.style.height = `${roundHeight}px`;
-    this._offsetHeight = roundHeight;
-  }
-
-  get offsetHeight(): number {
-    return this._offsetHeight;
-  }
-  
-  get offsetTop(): number {
-    return this._fillerElement.offsetTop;
   }
 }
 
