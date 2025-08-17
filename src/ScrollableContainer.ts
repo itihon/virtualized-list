4a1cec4ff74449285d4bdbc7b32a12969b8e447f
itihon <kalabin.alexandr@gmail.com>
2025-08-17 12:31:24 +0500

    refactor(ScrollableContainer.ts): move class BooleanToggle in a separate module

--- src/ScrollableContainer.ts
+++ src/ScrollableContainer.ts
@@ -1,46 +1,9 @@
 import ScrolledPane from './ScrolledPane';
 import Filler from './Filler';
+import BooleanToggle from './BooleanToggle';
 import './ScrollableContainer.css';
 
 export type OnScrollLimitCallback = (scrolledPane: ScrolledPane, scrollTop: number) => void;
-
-class BooleanToggle {
-  private _value = false;
-
-  constructor(initialValue: boolean = false) {
-    this._value = initialValue;
-  }
-
-  /**
-   * - value: false -> true, rising: true => true;
-   * - value: true -> false, rising: false => true;
-   * - otherwise returns false;
-   */
-  set(value: boolean, rising: boolean = false): boolean {
-    let result;
-
-    if (rising) {
-      if (!this._value && value) {
-        result = true;
-      }
-      else {
-        result = false;
-      }
-    }
-    else {
-      if (this._value && !value) {
-        result = true;
-      }
-      else {
-        result = false;
-      }
-    }
-
-    this._value = value;
-
-    return result;
-  }
-}
 
 export default class ScrollableContainer {
   private _scrollableParent: HTMLElement;
