b796ea598432fbe3c1e139ba6b170a1df2039e66
itihon <kalabin.alexandr@gmail.com>
2025-08-17 12:24:38 +0500

    refactor(ScrollableContainer.ts): move class ScrolledPane in a separate module

--- src/ScrollableContainer.ts
+++ src/ScrollableContainer.ts
@@ -1,3 +1,4 @@
+import ScrolledPane from './ScrolledPane';
 import './ScrollableContainer.css';
 
 export type OnScrollLimitCallback = (scrolledPane: ScrolledPane, scrollTop: number) => void;
@@ -64,48 +65,6 @@ class Filler {
   
   get offsetTop(): number {
     return this._fillerElement.offsetTop;
-  }
-}
-
-class ScrolledPane {
-  private _scrollableParent: HTMLElement;
-  private _paneElement: HTMLElement;
-  private _scrollDownLimit = 0;
-  private _scrollUpLimit = 0;
-
-  constructor(scrollableParent: HTMLElement) {
-    this._scrollableParent = scrollableParent;    
-    this._paneElement = document.createElement('div');
-    this._paneElement.classList.add('class__ScrolledPane');
-    this._scrollableParent.appendChild(this._paneElement);
-  }
-  
-  get offsetHeight(): number {
-    return this._paneElement.offsetHeight;
-  }
-  
-  get offsetTop(): number {
-    return this._paneElement.offsetTop;
-  }
-
-  set scrollDownLimit(limit: number) {
-    const roundLimit = Math.round(limit);
-    this._paneElement.style.setProperty('--scroll-down-limit', `${roundLimit}px`);
-    this._scrollDownLimit = roundLimit;
-  }
-  
-  set scrollUpLimit(limit: number) {
-    const roundLimit = Math.round(limit);
-    this._paneElement.style.setProperty('--scroll-up-limit', `${roundLimit}px`);
-    this._scrollUpLimit = roundLimit;
-  }
-
-  get scrollDownLimit(): number {
-    return this._scrollDownLimit;
-  }
-  
-  get scrollUpLimit(): number {
-    return this._scrollUpLimit;
   }
 }
 
