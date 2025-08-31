fe58015620de3577c5acd742b5a298f5e34d7532
itihon <kalabin.alexandr@gmail.com>
2025-08-31 16:45:51 +0500

    test: change manual tests

--- cypress/manual/scrollable-container.ts
+++ cypress/manual/scrollable-container.ts
@@ -1,40 +1,131 @@
 import ScrollableContainer from "../../src/ScrollableContainer";
 
-const INITIAL_ITEMS_NUMBER = 10;
+const nonVirtual = new URLSearchParams(window.location.search).get('nonVirtual');
+
+
+const INITIAL_ITEMS_NUMBER = 23;
+const itemsHeight = 40;
 const container = document.createElement('div');
-const scrollableContainer = new ScrollableContainer(container);
+
+if (nonVirtual) {
+
+  for (let i = 0; i < 1000; i++) {
+    const item = createItem(i);
+    container.append(item);
+  }
+
+  container.classList.add('class__ScrollableContainer');
+}
+else {
+  const scrollableContainer = new ScrollableContainer(container);
+  
+  scrollableContainer.setScrollHeight(40000);
+  
+  for (let i = 0; i < INITIAL_ITEMS_NUMBER; i++) {
+    const item = createItem(i);
+    scrollableContainer.append(item);
+  }
+
+  let i = 0;
+  scrollableContainer.onScrollDownOverscan((scrollTop, scrollLimit, padding, items, entry: IntersectionObserverEntry) => {
+    console.log('onScrollDownOverscan', items.length);
+
+    let addedItemsSize = 0;
+    let removedItemsSize = 0;
+
+    // const halfScrollLimit = scrollLimit / 2;
+    const freeSpace = entry.rootBounds!.bottom - entry.boundingClientRect.bottom;
+    // const scrollableContainerHeight = 300;
+    // const exceededSpace = entry.rootBounds!.top - entry.boundingClientRect.top;
+    // console.log('freeSpace', freeSpace, 'exceeded space', exceededSpace)
+    const scrolledPaneOffsetTop = (entry.target as HTMLElement).offsetTop;
+    
+    while (removedItemsSize < freeSpace) {
+      const item = items.item(0) as HTMLElement;
+      removedItemsSize += item.offsetHeight;
+      item.remove();
+    }
+
+    while (addedItemsSize < freeSpace) {
+      const item = createItem(i++);
+      scrollableContainer.append(item);
+      addedItemsSize += item.offsetHeight;
+    }
+
+
+    // console.log('scrolledPaneOffsetTop:', scrolledPaneOffsetTop, 'removedItemsSize:', removedItemsSize, 'addedItemsSize:', addedItemsSize, 'freeSpace', freeSpace, 'exceededSpace', exceededSpace);
+    // scrollableContainer.scroll(scrollTop - halfScrollLimit - (halfScrollLimit - removedItemsSize)); // 40:130+8 50:200+46 60:270+86 
+    // scrollableContainer.scroll(scrollTop - halfScrollLimit + removedItemsSize + (exceededSpace - removedItemsSize)); // 40:130+8 50:200+46 60:270+86 
+    scrollableContainer.scroll(scrolledPaneOffsetTop + removedItemsSize - padding);
+  });
+  // 
+  scrollableContainer.onScrollUpOverscan((scrollTop, scrollLimit, padding, items, entry) => {
+    console.log('onScrollUpOverscan', items.length);
+
+    let addedItemsSize = 0;
+    let removedItemsSize = 0;
+
+    // const halfScrollLimit = scrollLimit / 2;
+    const freeSpace = entry.boundingClientRect.top - entry.rootBounds!.top;
+    // const scrollableContainerHeight = 300;
+    // const exceededSpace = entry.rootBounds!.top - entry.boundingClientRect.top;
+    // console.log('freeSpace', freeSpace, 'exceeded space', exceededSpace)
+    const scrolledPaneOffsetTop = (entry.target as HTMLElement).offsetTop;
+    
+    while (removedItemsSize < freeSpace) {
+      const item = items.item(items.length - 1) as HTMLElement;
+      removedItemsSize += item.offsetHeight;
+      item.remove();
+    }
+
+    while (addedItemsSize < freeSpace) {
+      const item = createItem(i++);
+      scrollableContainer.prepend(item);
+      addedItemsSize += item.offsetHeight;
+    }
+
+
+    // console.log('scrolledPaneOffsetTop:', scrolledPaneOffsetTop, 'removedItemsSize:', removedItemsSize, 'addedItemsSize:', addedItemsSize, 'freeSpace', freeSpace, 'exceededSpace', exceededSpace);
+    // scrollableContainer.scroll(scrollTop - halfScrollLimit - (halfScrollLimit - removedItemsSize)); // 40:130+8 50:200+46 60:270+86 
+    // scrollableContainer.scroll(scrollTop - halfScrollLimit + removedItemsSize + (exceededSpace - removedItemsSize)); // 40:130+8 50:200+46 60:270+86 
+    scrollableContainer.scroll(scrolledPaneOffsetTop - removedItemsSize - padding);
+  });
+  // 
+  // scrollableContainer.onScrollDownOverflow((scrollTop, scrollLimit, items) => {
+  //   console.log('onScrollDownOverflow');
+  // });
+  // 
+  // scrollableContainer.onScrollUpOverflow((scrollTop, scrollLimit, items, entry) => {
+  //   console.log('onScrollUpOverflow');
+  //   
+  //   let removedItemsSize = 0;
+  //   const halfScrollLimit = scrollLimit / 2;
+  //   const scrollableContainerHeight = 300;
+  //   const exceededSpace = entry.boundingClientRect.height - (scrollableContainerHeight + entry.rootBounds!.height * 2);
+  //  
+  //   // while (removedItemsSize < exceededSpace) {
+  //   //   const item = items.item(0) as HTMLElement;
+  // 
+  //   //   if (item) {
+  //   //     removedItemsSize += item.offsetHeight;
+  //   //     item.remove();
+  //   //   }
+  //   // }
+  // 
+  //   // scrollableContainer.scroll(scrollTop - halfScrollLimit - removedItemsSize); // 40:130+8 50:200+46 60:270+86 
+  // });
+  // 
+  // // scrollableContainer.setOverscanHeight('300px');
+  scrollableContainer.setOverscanHeight('100%');
+}
 
 document.body.appendChild(container);
-scrollableContainer.setScrollHeight(20000);
 
 function createItem(i: number) {
   const item = document.createElement('div');
   item.classList.add('list-item');
-  item.style.height = '40px';
+  item.style.height = `${itemsHeight}px`;
   item.textContent = `Item #${i}.`;
   return item;
 }
 
-// for (let i = 0; i < INITIAL_ITEMS_NUMBER; i++) {
-//   const item = createItem(i);
-//   scrollableContainer.append(item);
-// }
-// 
-// let i = 0;
-// scrollableContainer.onScrollDownLimit((scrolledPane, scrollTop) => {
-//   scrolledPane.firstElementChild!.remove();
-//   scrolledPane.firstElementChild!.remove();
-//   const item1 = createItem(i++);
-//   const item2 = createItem(i++);
-//   scrollableContainer.append(item1);
-//   scrollableContainer.append(item2);
-// });
-// 
-
-scrollableContainer.onScrollDownLimit((scrolledPane, scrollTop) => {
-  scrollableContainer.scroll(scrollTop - 80);
-});
-
-scrollableContainer.onScrollUpLimit((scrolledPane, scrollTop) => {
-  scrollableContainer.scroll(scrollTop - 80);
-});
