ac0293da85da0e26e6b62ce285bf921a2d9a4712
itihon <kalabin.alexandr@gmail.com>
2025-09-18 18:18:03 +0500

    feat(scrolledpanebuffer.ts): add method for getting a reference to marker element

--- src/ScrolledPaneBuffer.ts
+++ src/ScrolledPaneBuffer.ts
@@ -3,16 +3,20 @@ import ScrolledPane from "./ScrolledPane";
 
 export default class ScrolledPaneBuffer extends ScrolledPane {
   private _bufferElement: HTMLElement;
+  private _markerElement: HTMLElement;
 
   constructor(scrollableParent: HTMLElement) {
     super(scrollableParent, ['class__ScrolledPaneBuffer']);
     this._bufferElement = super.DOMRoot;
-    this._bufferElement.append(
-      new DOMConstructor(
-        this._bufferElement, 
-        ['ScrolledPaneBuffer__Marker'],
-      ).DOMRoot,
-    );
+    this._markerElement = new DOMConstructor(
+      this._bufferElement, ['ScrolledPaneBuffer__Marker'],
+    ).DOMRoot;
+
+    this._bufferElement.append(this._markerElement);
+  }
+
+  getMarkerElement(): HTMLElement {
+    return this._markerElement;
   }
   
   get length(): number {
