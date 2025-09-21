b38e825499efa06715f314c570bb71dab631eb05
itihon <kalabin.alexandr@gmail.com>
2025-09-21 13:05:44 +0500

    feat(domconstructor.ts): add lifecycle callbacks

--- src/DOMConstructor.ts
+++ src/DOMConstructor.ts
@@ -1,15 +1,40 @@
+type LifecycleCallback = (element: DOMDivElement) => void;
+
+export class DOMDivElement extends HTMLDivElement {
+  private _onMountedCB: LifecycleCallback = () => {};
+  private _onUnmountedCB: LifecycleCallback = () => {};
+
+  connectedCallback() {
+    this._onMountedCB(this);
+  }
+
+  disconnectedCallback() {
+    this._onUnmountedCB(this)
+  }
+
+  onUnmounted(cb: LifecycleCallback) {
+    this._onUnmountedCB = cb;
+  }
+
+  onMounted(cb: LifecycleCallback) {
+    this._onMountedCB = cb;
+  }
+}
+
+customElements.define('domconstructor-div', DOMDivElement, { extends: 'div' });
+
 export default class DOMConstructor {
   private _parentContainer: HTMLElement;
-  private _DOMRoot: HTMLElement;
+  private _DOMRoot: DOMDivElement;
 
   constructor(parentContainer: HTMLElement, classList: string[]) {
     this._parentContainer = parentContainer;    
-    this._DOMRoot = document.createElement('div');
+    this._DOMRoot = new DOMDivElement();
     this._DOMRoot.classList.add(...classList);
     this._parentContainer.appendChild(this._DOMRoot);
   }
 
-  get DOMRoot(): HTMLElement {
+  get DOMRoot(): DOMDivElement {
     return this._DOMRoot;
   }
 
