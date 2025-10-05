a0c114483fe7e71d49793acde288b4cd37f49685
itihon <kalabin.alexandr@gmail.com>
2025-10-05 19:48:45 +0500

    test: add performance test

--- cypress/manual/flex-items-measurer.ts
+++ cypress/manual/flex-items-measurer.ts
@@ -10,19 +10,25 @@ const fim = new FlexItemsMeasurer(container);
 
 fim.offsetWidth = 200;
 fim.offsetHeight = 300;
-fim.setPortionSize(30);
+fim.setPortionSize(1000);
 
 
 fim.onPortionMeasured((flexRowsAcc) => {
-  console.log('on portion measured', flexRowsAcc.rows.map(row => row.map(entry => entry.target.textContent)));
-  console.log('first item:', fim.getFirstItem(), 'last item:', fim.getLastItem());
+  // console.log('on portion measured', flexRowsAcc.rows.map(row => row.map(entry => entry.target.textContent)));
+  // console.log('first item:', fim.getFirstItem()?.textContent, 'last item:', fim.getLastItem()?.textContent);
 });
+
+const startTime = performance.now();
 
 for (let i = 0; i < itemsCount; i++) {
   const item = document.createElement('div');
   item.textContent = `Item ${i}.`;
   fim.appendItem(item);
-  fim.measure();
 }
 
+fim.measure().then(() => {
+  document.body.append(
+    `Measuring of ${itemsCount} items took ${Math.round((performance.now() - startTime) / 1000)} seconds.`
+  );
+});
 
