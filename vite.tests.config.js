9cb29cc41fb1fc11f2f55aff714d069a5dc050d1
itihon <kalabin.alexandr@gmail.com>
2026-02-20 22:08:00 +0500

    chore: prepare test web server

--- vite.tests.config.js
+++ vite.tests.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   return {
-    root: './cypress/manual/',
+    root: './tests/fixtures/',
     server: {
       host: '0.0.0.0',
     },
