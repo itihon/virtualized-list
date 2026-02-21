2441f456aea3cc28f3cb240cfc39a8f13b8fa66a
itihon <kalabin.alexandr@gmail.com>
2026-02-21 17:45:14 +0500

    chore(vite.tests.config.js): configure fixed css class names to use in tests

--- vite.tests.config.js
+++ vite.tests.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   return {
+    css: {
+      modules: {
+        generateScopedName: '[local]',
+      },
+    },
     root: './tests/fixtures/',
     server: {
       host: '0.0.0.0',
