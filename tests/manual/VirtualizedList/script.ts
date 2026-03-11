4c2b726dd53c9af1584b1b2878b09f0c99f6be5c
itihon <kalabin.alexandr@gmail.com>
2026-03-11 14:29:26 +0500

    test: add manual test setup for virtualized list

---
+++ tests/manual/VirtualizedList/script.ts
@@ -0,0 +1,27 @@
+import VirtualizedList from '../../../src/VirtualizedList/VirtualizedList';
+import FixedListLayout from '../../../src/Layout/FixedListLayout';
+import ArrayItemStore from '../../../src/ItemStore/ArrayItemStore';
+
+function render(data: { i: number } | unknown) {
+  const item = document.createElement('div');
+  item.textContent = `Item ${(data as { i: number }).i}.`
+  return item;
+}
+
+const store = new ArrayItemStore();
+const layout = new FixedListLayout();
+const container = document.createElement('div');
+container.style.width = '200px';
+container.style.height = '300px';
+container.style.overflow = 'auto';
+
+document.body.appendChild(container);
+const list = new VirtualizedList({ layout, store, container });
+
+for (let i = 0; i < 10000; i++) {
+  list.insert({
+    data: { i: i },
+    render,
+    height: 40,
+  }, i);
+}
