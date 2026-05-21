9911690a89ed120bff410a2d70d81d04d7a10380
itihon <kalabin.alexandr@gmail.com>
2026-05-21 12:53:35 +0500

    chore(homepage): add CI deploy script

--- homepage/astro.config.mjs
+++ homepage/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://itihon.github.io',
+  base: '/layout-virtual',
 	integrations: [
 		starlight({
       favicon: './src/assets/hero-image-1.png',
